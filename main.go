5bc6eb9a233cbc617b77ee0935ce7d0cf60dc93f
yori <yori@uncinc.nl>
2017-01-21 12:14:01 +0100

    Declaring at the Package Level

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var (
+	name, course string
+	module       float64
+)
+
+func main() {
+	fmt.Println("Name is set to", name)
+	fmt.Println("Name is set to", module)
+}
