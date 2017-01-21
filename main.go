69b4eeb3e2cc7da39181fc810e4b66caf683cb22
yori <yori@uncinc.nl>
2017-01-21 12:43:26 +0100

    Short Assignment

--- main.go
+++ main.go
@@ -5,23 +5,30 @@ import (
 	"reflect"
 )
 
-var (
-	// declaring types u can do like this
-
-	// name, course string
-	// module       float64
-
-	// or like this
-
-	// name, course, module = "nigel", "Docker Deep Dive", 3.2
-
-	// but this is more readable
-	name   = "nigel"
-	course = "Docker Deep Dive"
-	module = 3.2
-)
+// Package level variables are global
+// var (
+// 	// declaring types u can do like this
+//
+// 	// name, course string
+// 	// module       float64
+//
+// 	// or like this
+//
+// 	// name, course, module = "nigel", "Docker Deep Dive", 3.2
+//
+// 	// but this is more readable
+// 	name   = "nigel"
+// 	course = "Docker Deep Dive"
+// 	module = 3.2
+// )
 
 func main() {
+	// This declaring only works in function.
+	// Variables at the function level must be used.
+	name := "nigel"
+	// course := "Docker Deep Dive"
+	module := 3.2
+
 	fmt.Println("Name is set to", name, "and is of type ", reflect.TypeOf(name))
 	fmt.Println("Name is set to", module, "and is of type ", reflect.TypeOf(module))
 }
